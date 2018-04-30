509c8e1bb4db8448f13180f139feb068b2c66905
PredaG <38722442+PredaG@users.noreply.github.com>
2018-04-30 15:32:14 +0300

    Natural numbers

---
+++ src/com/pluralsight/exercises/Exercises2.java
@@ -0,0 +1,13 @@
+package com.pluralsight.exercises;
+
+public class Exercises2 {
+    public static void main(String[] args) {
+        int i;
+        System.out.println ("The first 10 natural numbers are:");
+        for (i=1;i<=10;i++)
+        {
+            System.out.println (i);
+        }
+
+    }
+}
